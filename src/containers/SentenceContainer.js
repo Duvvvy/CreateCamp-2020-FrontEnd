6b92b73b2a38f5f559715f38fd33035bad1636f5
Duvvvy <ryanbircham@gmail.com>
2020-08-02 12:04:43 +1200

    Link to maps

--- src/containers/SentenceContainer.js
+++ src/containers/SentenceContainer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import LocalisedButton from '../components/buttons/LocalisedButton';
 import axios from 'axios';
 import  "./SentenceContainers.css";
@@ -32,84 +32,91 @@ export function SentenceContainer() {
     getData();
   },[])
 
-  function randomInt(length){
-    return Math.floor(Math.random() * length);
+  // function randomInt(length){
+  //   return Math.floor(Math.random() * length);
+  // }
+
+  function getMapToDestination(){
+    return "https://www.google.com/maps/search/?api=1&query=" + suggestion.latitude + "," + suggestion.longitude
   }
 
-
-  if(suggestion){
-    return (
-        <div className = "sentenceContainer">
-          <div className = "wholeSentenceText">
-            <span className = "sentenceText">Why not try </span>
-            <span className = "sentenceText"><a href={suggestion.zenbulink}> {suggestion.name} </a> </span>
-            </div>
-
-            <div>
-            {/* <p className = "sentenceText">{suggestion[count].address},</p> */}
-            <p className = "sentenceText"><i>{suggestion.tags}</i></p> 
-            <p className = "sentenceText">they are only {suggestion.distance} km away</p>
+  return (
+      <div className = "sentenceContainer">
+        {(suggestion && !isLoading) && 
+          <Fragment>
+        <div className = "wholeSentenceText">
+          <span className = "sentenceText">Why not try </span>
+          <span className = "sentenceText"><a href={suggestion.zenbulink}> {suggestion.name} </a> </span>
           </div>
 
-        <div>
-        <p className="">Price: {suggestion.price}</p>
-    <p className="">Rating: {suggestion.rating}</p>
+          <div>
+          {/* <p className = "sentenceText">{suggestion[count].address},</p> */}
+          <p className = "sentenceText"><i>{suggestion.tags}</i></p> 
+          <p className = "sentenceText">they are only {suggestion.distance} km away</p>
         </div>
 
-          <div className = "yesNoButtons"> 
+      <div>
+      <p className="">Price: {suggestion.price}</p>
+      <p className="">Rating: {suggestion.rating}</p>
+      </div>
 
-          {/* Yes Button */}
-          <div className="row justify-content-center"><LocalisedButton className = "Yes"
-          LocalSlang={
-          [
-          "Sweet as!",
-          "Lesh go!",
-          "ya yeet!",
-          "Choice",
-          "Āe",
-          "Yea!",
-          "Keen as",
-          "Lets get it",
-          "Aight then",
-          "heck yea",
-          "I want it",
-          "Love it",
-          "Aye aye, captain!",
-          "Yes, yes, and yes!",
-          "Straight up g!",
-          "Deal!",
-          "Chur bro"
-          ]
-          }
-          onClick={() => console.log("yes option not yet implemented")}
-          />
-          
-          {/* No Button */}
-          
-          
-          <LocalisedButton className = "No"
-          LocalSlang={
-          [
-          "Nah g",
-          "Not the play",
-          "ceebs",
-          "Gap it",
-          "Not even"
-          ]
-          }
-          onClick={() => getData()}
-          
-          /></div>
-          </div>
-    </div>
+        <div className = "yesNoButtons"> 
 
+        {/* Yes Button */}
+        <div className="row justify-content-center">
+          <LocalisedButton 
+          href={getMapToDestination()}
+          className = "Yes"
+        LocalSlang={
+        [
+        "Sweet as!",
+        "Lesh go!",
+        "ya yeet!",
+        "Choice",
+        "Āe",
+        "Yea!",
+        "Keen as",
+        "Lets get it",
+        "Aight then",
+        "heck yea",
+        "I want it",
+        "Love it",
+        "Aye aye, captain!",
+        "Yes, yes, and yes!",
+        "Straight up g!",
+        "Deal!",
+        "Chur bro"
+        ]
+        }
+        onClick={() => window.open(getMapToDestination())}
+        />
+        
+        {/* No Button */}
+        
+        
+        <LocalisedButton className = "No"
+        LocalSlang={
+        [
+        "Nah g",
+        "Not the play",
+        "ceebs",
+        "Gap it",
+        "Not even"
+        ]
+        }
+        onClick={() => getData()}
+        
+        /></div>
+        </div>
+        </Fragment>
+        } 
+        {(suggestion == null && isLoading)&&
+          <p>Loading...</p>
+        }
+        {(suggestion == null && !isLoading) && 
+          <div>unluggy uce no locations near you. Don't pack a sad, try us out again when you are closer to town! <span role="img" aria-label="sad face">😭</span></div>
+        }
+      </div>
   );
-  } else if( isLoading ) {
-    return <p>Loading...</p>
-  }
-    else{
-    return(
-      <div>unluggy uce no locations near you. Don't pack a sad, try us out again when you are closer to town! <span role="img" aria-label="sad face">😭</span></div>
-    )
-  }
+
 }
